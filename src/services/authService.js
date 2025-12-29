3d96e26f69d62b630df8584ea6ccb7eac3d92ea9
Ankit Kumar <itskumarankit52@gmail.com>
2025-12-29 13:08:39 +0530

    changes in auth

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/auth";
+const API = "https://ai-interview-platform-backend-1.onrender.com";
 
 export const loginUser = (data) =>
   axios.post(`${API}/login`, data);
